a2dc4af9260db4de452c3fc3a64f691e513ff841
Björn Westlin <bwestlin@gmail.com>
2018-12-20 00:39:30 +0100

    Add input re-implementation and comments

--- src/day19/day19.rs
+++ src/day19/day19.rs
@@ -177,6 +177,92 @@ fn print_instructions(instructions: &Vec<Instruction>, ipb: usize) {
     }
 }
 
+// Re-implementation of the code found in the input
+fn input_reimpl(a: u32) -> u32 {
+    // Disassembly of input with comments
+    //
+    // Ia OpCode A   B   C      Explained
+    //  0 Addi,  4, 16,  4  =>  e(ip) = e(ip) + 16               // Goto 17
+    //  1 Seti,  1,  5,  1  =>  b = 1
+    //  2 Seti,  1,  7,  3  =>  d = 1
+    //  3 Mulr,  1,  3,  5  =>  f = b * d
+    //  4 Eqrr,  5,  2,  5  =>  f = if f == c { 1 } else { 0 }
+    //  5 Addr,  5,  4,  4  =>  e(ip) = f + e(ip)                // Goto 7 if f == c
+    //  6 Addi,  4,  1,  4  =>  e(ip) = e(ip) + 1                // Goto 8 if f != c
+    //  7 Addr,  1,  0,  0  =>  a = b + a
+    //  8 Addi,  3,  1,  3  =>  d = d + 1
+    //  9 Gtrr,  3,  2,  5  =>  f = if d > c { 1 } else { 0 }
+    // 10 Addr,  4,  5,  4  =>  e(ip) = e(ip) + f                // Goto 12 if d > c
+    // 11 Seti,  2,  4,  4  =>  e(ip) = 2                        // Goto 3 if d <= c
+    // 12 Addi,  1,  1,  1  =>  b = b + 1
+    // 13 Gtrr,  1,  2,  5  =>  f = if b > c { 1 } else { 0 }
+    // 14 Addr,  5,  4,  4  =>  e(ip) = f + e(ip)                // Goto 16 if b > c (Effectively Exit)
+    // 15 Seti,  1,  5,  4  =>  e(ip) = 1                        // Goto 2 if b <= c
+    // 16 Mulr,  4,  4,  4  =>  e(ip) = e(ip) * e(ip)            // Goto 257 (Exit)
+    // 17 Addi,  2,  2,  2  =>  c = c + 2
+    // 18 Mulr,  2,  2,  2  =>  c = c * c
+    // 19 Mulr,  4,  2,  2  =>  c = e(ip) * c
+    // 20 Muli,  2, 11,  2  =>  c = c * 11
+    // 21 Addi,  5,  2,  5  =>  f = f + 2
+    // 22 Mulr,  5,  4,  5  =>  f = f * e(ip)
+    // 23 Addi,  5, 18,  5  =>  f = f + 18
+    // 24 Addr,  2,  5,  2  =>  c = c + f
+    // 25 Addr,  4,  0,  4  =>  e(ip) = e(ip) + a
+    // 26 Seti,  0,  6,  4  =>  e(ip) = 0                        // Goto 1 if a == 0
+    // 27 Setr,  4,  3,  5  =>  f = e(ip)
+    // 28 Mulr,  5,  4,  5  =>  f = f * e(ip)
+    // 29 Addr,  4,  5,  5  =>  f = e(ip) + f
+    // 30 Mulr,  4,  5,  5  =>  f = e(ip) * f
+    // 31 Muli,  5, 14,  5  =>  f = f * 14
+    // 32 Mulr,  5,  4,  5  =>  f = f * e(ip)
+    // 33 Addr,  2,  5,  2  =>  c = c + f
+    // 34 Seti,  0,  2,  0  =>  a = 0
+    // 35 Seti,  0,  6,  4  =>  e(ip) = 0                        // Goto 1
+
+    let (mut a, mut b, mut c, mut d, e, mut f) = (a, 0, 0, 0, 0, 0);
+
+    c += 2;
+    c *= c;
+    c *= 19;
+    c *= 11;
+    f += 2;
+    f *= 22;
+    f += 18;
+    c += f;
+    if a == 1 {
+        f = 27;
+        f *= 28;
+        f += 29;
+        f *= 30;
+        f *= 14;
+        f *= 32;
+        c += f;
+        a = 0;
+    }
+
+    b = 1;
+    d = 1;
+    loop {
+        f = b * d;
+        if f == c {
+            a += b;
+        }
+        d = d + 1;
+        if d > c {
+            b = b + 1;
+            if b > c {
+                break;
+            }
+            d = 1;
+        }
+    }
+    a
+}
+
+fn sum_divisors(n: u32) -> u32 {
+    (1..=n).filter(|i| n % i == 0).sum()
+}
+
 fn part1(input: &Vec<String>) -> u32 {
     let (ipb, instructions) = parse_input(input);
     #[cfg(feature = "print")] print_instructions(&instructions, ipb);
@@ -194,14 +280,18 @@ fn part2(input: &Vec<String>) -> u32 {
     let mut d = Device::new(ipb);
     d.registers[0] = 1;
 
+    // Let the program run til it reaches ip=1
     d.run_to(1, &instructions);
 
-    // Assume value is in register 3 (C)
-    sum_divisors(d.registers[2])
-}
+    // Now we can assume the value to use is in register 3 (C)
+    let value = d.registers[2];
 
-fn sum_divisors(n: u32) -> u32 {
-    (1..=n).filter(|i| n % i == 0).sum()
+    let fast = true;
+    if fast {
+        sum_divisors(value)
+    } else {
+        input_reimpl(1)
+    }
 }
 
 fn main() -> Result<(), Box<Error>> {
